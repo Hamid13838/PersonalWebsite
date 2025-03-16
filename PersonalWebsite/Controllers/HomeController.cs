e4ccd02ae16120870b09f4768503198729995431
Hamid <hamid13838@gmail.com>
2025-03-17 00:48:02 +0330

    Add Views

--- PersonalWebsite/Controllers/HomeController.cs
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace PersonalWebsite.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
